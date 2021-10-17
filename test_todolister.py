8a5ab7a5d4b2815f31ec31f7ad5ab59194127433
Bill Melvin <bill@billmelvin.com>
2021-10-16 21:12:58 -0400

    Add test that uses pytest tmp_path fixture. Arguments list passed as todolister.main(args).

--- test_todolister.py
+++ test_todolister.py
@@ -1,17 +1,29 @@
 
-import sys
+import textwrap
+
 import todolister
-
-
-# def test_1(capsys):
-#     sys.stderr.write("blah\n")
-#     a = capsys.readouterr()
-#     assert a.err == "blah\n"
-#     # assert False
-#     # todolister
 
 
 def test_1():
     args = ["todolister.py", "-f", "todolister.opt", "--no-browser"]
+    result = todolister.main(args)
+    assert result == 0
+
+
+def test_2(tmp_path):
+    test_dir = tmp_path / "todo"
+    test_dir.mkdir()
+    test_file = test_dir / "notes-test2.txt"
+    test_file.write_text(
+        textwrap.dedent(
+            """
+            [ ] Do this thing.
+
+            [x] ...but not this thing, because it's already done.
+            """
+        )
+    )
+    args = ["todolister.py", str(test_dir), "--no-browser"]
+    # args = ["todolister.py", str(test_dir)]
     result = todolister.main(args)
     assert result == 0
